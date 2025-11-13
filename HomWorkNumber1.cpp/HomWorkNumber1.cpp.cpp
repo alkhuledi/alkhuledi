e62533230ce0d564381de625496a4091ffd7208e
alkhuledi <124508318+alkhuledi@users.noreply.github.com>
2025-11-13 23:55:03 +0300

    Aded new file cpp homeworke number 1

---
+++ HomWorkNumber1.cpp/HomWorkNumber1.cpp.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+using namespace std;
+//Wrate a Program to print Your Nsme on Screen
+
+string  printname(string name)
+{
+    cout << "your name is: " << name << endl;
+    return name;
+
+}
+int main()
+{
+    printname("alkhuledi");
+   
+}
